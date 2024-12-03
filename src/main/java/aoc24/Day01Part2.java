18a165eb609c2a72ab4fd2e80d34d3303942fd7f
Alejandro Blanco <alexbl1996@gmail.com>
2024-12-03 19:36:04 +0100

    Day 01 part 2

---
+++ src/main/java/aoc24/Day01Part2.java
@@ -0,0 +1,28 @@
+package aoc24;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Day01Part2 {
+    void main() {
+        String input = Utils.readFile("/inputs/01-input.txt");
+
+        List<Integer> first = input.lines()
+                .map(line -> line.split("\\s")[0])
+                .map(Integer::valueOf)
+                .toList();
+
+        List<Integer> second = input.lines()
+                .map(line -> line.split("\\s")[3])
+                .map(Integer::valueOf)
+                .toList();
+
+        int sum = 0;
+        for (int i : first) {
+            int count = (int) second.stream().filter(j -> Objects.equals(i, j)).count();
+            sum += i * count;
+        }
+
+        System.out.println(sum);
+    }
+}
